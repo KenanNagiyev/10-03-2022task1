e7ad06d1ebc3b1ea11c4b8bb37f557dbb4665525
KenanNagiyev <788lnmp@code.edu.az>
2022-03-10 11:26:53 +0400

    new file

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 3, 8, 5, 1, 6, 9, 7 };
+            Console.WriteLine(MinMax(nums));
+        }
+        static int MinMax(int[] nums)
+        {
+
+            int min = nums[0];
+            int max = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min)
+                {
+
+                    min = nums[i];
+                    Console.WriteLine(nums[i]);
+                }
+                if (nums[i] > max)
+                {
+
+                    max = nums[i];
+                    Console.WriteLine(nums[i]);
+                }
+
+                min = max;
+            }
+            return min ;
+        }
+        }
+}
